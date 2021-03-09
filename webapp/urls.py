c766f0fed0ae31b284fc4c0e933ba0fb3a593d88
Sharika Anjum Mondal <54097782+sharika-anjum@users.noreply.github.com>
2021-03-09 19:58:55 +0530

    Add files via upload

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
 	path('', views.open),
 	path('home/', views.land),
+	path('check', views.check),
+	path('log_check', views.log_check),
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User,auth
 from django.contrib import messages,auth
+
+from .models import *
+from .forms import *
 
 from .models import *
 from .forms import *
@@ -12,3 +14,36 @@ def land(request):
 def open(request):
     return render(request,"index.html")
 
+def check(request):
+
+    if request.method=='POST' and 'Sign up' in request.POST:
+        form=UserForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('')
+            except:
+                pass
+    else:
+        form=UserForm()
+    return render(request,'index.html',{'form':form})
+
+def log_check(request):
+    if request.method=="POST" and 'Login' in request.POST:
+
+        name = request.POST['name']
+        pwd = request.POST['pwd']
+        try:
+            u=user.objects.get(name=name,pwd=pwd)
+            if u is not None:
+                return redirect("home/")
+
+
+            else:
+                messages.info(request,'INVALID CREDENTIALS!')
+                return redirect('/')
+        except:
+            messages.info(request, 'INVALID CREDENTIALS!')
+            return redirect('/')
+    else:
+        return render(request,'login.html')
